b911c8a259ce36af9175e7af09fb77b820cd6d87
Pearl Law <pearllynnelaw@gmail.com>
2018-10-03 15:12:08 -0700

    Create a navigation bar that includes links to the homepage (Cards) and flashcards (New) #2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Home from './homepage'
+import Navigation from './navbar'
 
 ReactDOM.render(
-  <Home />,
+  <Navigation />,
   document.querySelector('#root')
 )
---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="#">Cards</a>
+          <a className="nav-item nav-link" href="#new">New</a>
+        </div>
+      </div>
+    </nav>
+  )
+}
