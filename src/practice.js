78326422c01cad751ac19646ac9903b175ba80f8
Pearl Law <pearllynnelaw@gmail.com>
2018-10-08 15:58:03 -0700

    Create a button with the Show Answer message #6

--- src/practice.js
+++ src/practice.js
@@ -4,16 +4,25 @@ export default class Practice extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      currentIndex: 0
+      currentIndex: 0,
+      showAnswer: 'hidden'
     }
+    this.showAnswer = this.showAnswer.bind(this)
+  }
+
+  showAnswer() {
+    this.setState({ showAnswer: ''})
   }
 
   render() {
     const { cards } = this.props
     const { currentIndex } = this.state
       return (
-        <div className="card-body text-center mt-5 mx-5 pt-5 border">
-          <h5 className="card-title">{cards[currentIndex].question}</h5>
+        <div className="card w-75">
+          <div className="card-body border">
+            <h5 className="card-title">{cards[currentIndex].question}</h5>
+            <i className="fas fa-chevron-circle-right" onClick={this.showAnswer}> Show Answer</i>
+          </div>
         </div>
       )
   }
