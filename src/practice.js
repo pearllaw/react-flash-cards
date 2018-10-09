e510baf2a7f68bd3eadd20a7e4fcb46b908c91ea
Pearl Law <pearllynnelaw@gmail.com>
2018-10-09 09:26:27 -0700

    Create forward and back arrows #6

--- src/practice.js
+++ src/practice.js
@@ -1,5 +1,22 @@
 import React, {Component} from 'react'
 
+const styles={
+  container: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)'
+  },
+  rightArrow: {
+    position: 'absolute',
+    top: '50px',
+    left: '10px'
+  },
+  leftArrow: {
+    position: 'absolute',
+  }
+}
+
 export default class Practice extends Component {
   constructor(props) {
     super(props)
@@ -18,12 +35,18 @@ export default class Practice extends Component {
     const { cards } = this.props
     const { currentIndex, showAnswer } = this.state
       return (
-        <div className="card w-75">
-          <div className="card-body border">
-            <h5 className="card-title">{cards[currentIndex].question}</h5>
-            <i className={showAnswer === false ? "fas fa-chevron-circle-right" : "fas fa-chevron-circle-down"} onClick={this.toggleAnswer}> Show Answer</i>
-            <p className="card-text" style={{visibility: showAnswer === false ? 'hidden' : ''}}>{cards[currentIndex].answer}</p>
+        <div>
+          <div className="container" style={styles.container}>
+            <div className="card w-50">
+              <div className="card-body border">
+                <h5 className="card-title">{cards[currentIndex].question}</h5>
+                <i className={showAnswer === false ? "fas fa-chevron-circle-right" : "fas fa-chevron-circle-down"} onClick={this.toggleAnswer}> Show Answer</i>
+                <p className="card-text" style={{visibility: showAnswer === false ? 'hidden' : ''}}>{cards[currentIndex].answer}</p>
+              </div>
+            </div>
           </div>
+          <i className="fas fa-chevron-left fa-3x"></i>
+          <i className="fas fa-chevron-right fa-3x"></i>
         </div>
       )
   }
